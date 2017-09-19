086e2dc4f20282b32a5be6fa16433022dc8e1fe6
Nadim Uddin <nadim.uddin1995@gmail.com>
2017-09-19 12:53:40 -0400

    add render view for html and integrate nunjucks

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require( 'express');
+const nunjucks = require('nunjucks');
+
 const app = express(); // creates an instance of an express application
 
 let port =3000;
 
+
+
+
 app.use(function(req, res, next){
 	console.log(req.method, res.statusCode);
 	console.log(req.path);
+
 	next();
 });
 
@@ -15,10 +21,19 @@ app.use('/special',function(req, res, next){
 });
 
 app.get('/', function(req, res){
-	res.send('WELCOME TO TWITTER.JS POWERED BY SERVERS IN NORWAY');
+	//res.send('WELCOME TO TWITTER.JS POWERED BY SERVERS IN NORWAY');
+	const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+	res.render( 'index', {title: 'Hall of Fame', people: people} );
+
 });
 
 app.listen(port, function(req, res){
 	console.log('Server is running...');
+
 });
+
+app.set('view engine', 'html'); // have res.render work with html files
+app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
+nunjucks.configure('views', {noCache: true}); // point nunjucks to the proper directory for templates
+
 
